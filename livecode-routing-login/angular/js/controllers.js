8e4ae4969e521d80cc63ae55b626b26048339ea9
Martin Lehmann <lehmar11@student.westerdals.no>
2015-03-16 14:16:13 +0100

    login from html

--- livecode-routing-login/angular/js/controllers.js
+++ livecode-routing-login/angular/js/controllers.js
@@ -8,29 +8,22 @@ app.config(function($routeProvider) {
 			controller: 'MainController',
 			templateUrl: 'templates/main.html'
 		})
+		.when('/login', {
+			controller: 'LoginController',
+			templateUrl: 'templates/login.html'
+		})
 		.otherwise({
 			controller: 'NotFoundController',
 			templateUrl: 'templates/not_found.html'
 		});
 });
 
-app.controller('NotFoundController', function($scope) {
-	$scope.message = 'Not found!';
-});
-
-app.controller('MainController', function($scope, LoginService, UserService) {
-	LoginService.login('theneva', 'ananas')
-		.then(function() {
-			UserService.findAll()
-				.success(function(users) {
-					$scope.users = users;
-				});
-		});
-});
-
-app.service('UserService', function($http) {
-	this.findAll = function() {
-		return $http.get('/api/users');
+app.controller('LoginController', function($scope, $location, LoginService) {
+	$scope.login = function(username, password) {
+		LoginService.login(username, password)
+			.then(function() {
+				$location.path('/');
+			});
 	};
 });
 
@@ -39,6 +32,28 @@ app.service('LoginService', function($http) {
 		return $http.post('/api/sessions', {username: username, password: password})
 			.success(function(token) {
 				$http.defaults.headers.common.Authorization = token;
+			})
+			.error(function(message) {
+				// Alert the user
+				console.log(message);
 			});
+	};
+});
+
+
+app.controller('NotFoundController', function($scope) {
+	$scope.message = 'Not found!';
+});
+
+app.controller('MainController', function($scope, UserService) {
+	UserService.findAll()
+		.success(function(users) {
+			$scope.users = users;
+		});
+});
+
+app.service('UserService', function($http) {
+	this.findAll = function() {
+		return $http.get('/api/users');
 	};
 });
