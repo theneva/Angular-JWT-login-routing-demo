b9fb3d0626d322b76655e07201475f2cf2618187
Martin Lehmann <lehmar11@student.westerdals.no>
2015-03-16 13:51:14 +0100

    Login from angular.

--- livecode-routing-login/angular/js/controllers.js
+++ livecode-routing-login/angular/js/controllers.js
@@ -1,15 +1,27 @@
 var app = angular.module('loginDemo', [
 ]);
 
-app.controller('MainController', function($scope, UserService) {
-	UserService.findAll()
-		.success(function(users) {
-			$scope.users = users;
+app.controller('MainController', function($scope, LoginService, UserService) {
+	LoginService.login('theneva', 'ananas')
+		.then(function() {
+			UserService.findAll()
+				.success(function(users) {
+					$scope.users = users;
+				});
 		});
 });
 
 app.service('UserService', function($http) {
 	this.findAll = function() {
 		return $http.get('/api/users');
+	};
+});
+
+app.service('LoginService', function($http) {
+	this.login = function(username, password) {
+		return $http.post('/api/sessions', {username: username, password: password})
+			.success(function(token) {
+				$http.defaults.headers.common.Authorization = token;
+			});
 	};
 });
