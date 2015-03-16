8fedb5c9b0059f6772b171c8684f4fc913207f9a
Martin Lehmann <lehmar11@student.westerdals.no>
2015-03-16 13:43:32 +0100

    Secure users resource.

--- livecode-routing-login/server/login-demo.js
+++ livecode-routing-login/server/login-demo.js
@@ -1,7 +1,10 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var jwt = require('jwt-simple');
 
 var app = express();
+
+var secret = 'westerdalsosloschoolofartscommunicationandtechnologyavdelingforteknologicampusgalleriet';
 
 app.use('/', express.static(__dirname + '/../angular'));
 app.use(bodyParser.json());
@@ -10,7 +13,35 @@ app.get('/api', function(req, res) {
 	res.send('Hello, world');
 });
 
+app.post('/api/sessions', function(req, res) {
+	var loginAttempt = req.body;
+
+	if (!loginAttempt
+			|| !loginAttempt.username
+			|| !loginAttempt.password) {
+		return res.status(401).send('Request must contain {username, password}');
+	}
+
+	if (loginAttempt.username !== 'theneva'
+			|| loginAttempt.password !== 'ananas') {
+		return res.status(401).send('Wrong username or password');
+	}
+
+	var payload = {};
+	var token = jwt.encode(payload, secret);
+
+	return res.status(201).send(token);
+});
+
 app.get('/api/users', function(req, res) {
+	var token = req.header('authorization');
+
+	if (!token) {
+		return res.status(401).send('No token supplied');
+	}
+
+	var payload = jwt.decode(token, secret);
+
 	res.json([
 		{username: 'theneva', phone: '32478972'},
 		{username: 'eivindveg', phone: '78493893'},
