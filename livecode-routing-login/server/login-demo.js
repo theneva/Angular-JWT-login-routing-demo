1706e1f5cc586f5b31f1ddd4b3c42d19cdc3ad89
Martin Lehmann <lehmar11@student.westerdals.no>
2015-03-16 13:04:34 +0100

    Serve simple static HTML thing

--- livecode-routing-login/server/login-demo.js
+++ livecode-routing-login/server/login-demo.js
@@ -1,9 +1,21 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 
 var app = express();
 
+app.use('/', express.static(__dirname + '/../angular'));
+app.use(bodyParser.json());
+
 app.get('/api', function(req, res) {
 	res.send('Hello, world');
+});
+
+app.get('/api/users', function(req, res) {
+	res.json([
+		{username: 'theneva'},
+		{username: 'eivindveg'},
+		{username: 'j0nas'}
+	]);
 });
 
 var port = 6783;
