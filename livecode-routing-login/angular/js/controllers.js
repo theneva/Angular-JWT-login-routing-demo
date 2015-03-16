a44cbce9ee2b72ca3bc98ac6e82bb67f6e429143
Martin Lehmann <lehmar11@student.westerdals.no>
2015-03-16 13:11:37 +0100

    Fetch users and display them in table

--- livecode-routing-login/angular/js/controllers.js
+++ livecode-routing-login/angular/js/controllers.js
@@ -1,6 +1,15 @@
 var app = angular.module('loginDemo', [
 ]);
 
-app.controller('MainController', function($scope) {
-	$scope.message = 'hi!';
+app.controller('MainController', function($scope, UserService) {
+	UserService.findAll()
+		.success(function(users) {
+			$scope.users = users;
+		});
+});
+
+app.service('UserService', function($http) {
+	this.findAll = function() {
+		return $http.get('/api/users');
+	};
 });
