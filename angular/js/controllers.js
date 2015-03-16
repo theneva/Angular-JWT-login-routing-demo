0c5664a938ef2296e29cc64eb3e8d3de7a8feb00
Martin Lehmann <lehmar11@student.westerdals.no>
2015-03-16 10:40:05 +0100

    Add basic jwt support without username/password

--- angular/js/controllers.js
+++ angular/js/controllers.js
@@ -1,12 +1,26 @@
 var app = angular.module('routingLoginDemo', [
 ]);
 
-app.controller('MainController', function($scope, PersonService) {
-	PersonService.findAll()
-		.success(function(people) {
-			$scope.people = people;
-		});
+app.controller('MainController', function($scope, LoginService, PersonService) {
+	LoginService.login()
+		.then(function() {
+			PersonService.findAll()
+				.success(function(people) {
+					$scope.people = people;
+				});
+		})
 });
+
+app.service('LoginService', function($http) {
+	this.login = function() {
+		return $http.post('/sessions')
+			.then(function(response) {
+				var token = response.data;
+				$http.defaults.headers.common.Authorization = token;
+			});
+	};
+});
+
 
 app.service('PersonService', function($http) {
 	this.findAll = function() {
--- server/routing-login-server.js
+++ server/routing-login-server.js
@@ -1,7 +1,9 @@
 var express = require('express'),
+	jwt = require('jwt-simple'),
     bodyParser = require('body-parser');
 
 var app = express();
+var secret = 'some long secret';
 
 app.use(bodyParser.json());
 app.use('/', express.static(__dirname + '/../angular'));
@@ -10,7 +12,20 @@ app.get('/api', function(req, res) {
     res.send('Hello, world!');
 });
 
+app.post('/sessions', function(req, res) {
+	var token = jwt.encode({}, secret);
+	res.status(201).send(token);
+});
+
 app.get('/api/people', function(req, res) {
+	var token = req.header('authorization');
+
+	if (!token) {
+		return res.status(401).send('No token supplied');
+	}
+
+	var payload = jwt.decode(token, secret);
+
 	res.json([
 		{username: 'Theneva'},
 		{username: 'theknarf'},
