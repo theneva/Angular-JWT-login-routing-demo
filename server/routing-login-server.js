96885446cf61af3b409459eb24befb4f54ab8588
Martin Lehmann <lehmar11@student.westerdals.no>
2015-03-16 09:06:03 +0100

    Initial project.

---
+++ server/routing-login-server.js
@@ -0,0 +1,15 @@
+var express = require('express'),
+    bodyParser = require('body-parser');
+
+var app = express();
+
+app.use(bodyParser.json());
+app.use('/', express.static(__dirname + '/../angular'));
+
+app.get('/api', function(req, res) {
+    res.send('Hello, world!');
+});
+
+var port = 7894;
+app.listen(port);
+console.log('routing-login-server listening on port ' + port);
