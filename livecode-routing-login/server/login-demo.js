2b1cf014ad14ae4a56c4e54e6d4af18fa88af1fd
Martin Lehmann <lehmar11@student.westerdals.no>
2015-03-16 14:45:29 +0100

    Fetch user form mongodb when authenticating

--- livecode-routing-login/server/login-demo.js
+++ livecode-routing-login/server/login-demo.js
@@ -1,10 +1,20 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var jwt = require('jwt-simple');
+var mongoose = require('mongoose');
 
 var app = express();
 
 var secret = 'westerdalsosloschoolofartscommunicationandtechnologyavdelingforteknologicampusgalleriet';
+
+mongoose.connect('mongodb://localhost/loginDemo', function() {
+	console.log('Connected to MongoDB');
+});
+
+var User = mongoose.model('User', {
+	username: String,
+	password: String
+});
 
 app.use('/', express.static(__dirname + '/../angular'));
 app.use(bodyParser.json());
@@ -22,25 +32,29 @@ app.post('/api/sessions', function(req, res) {
 		return res.status(401).send('Request must contain {username, password}');
 	}
 
-	if (loginAttempt.username !== 'theneva'
-			|| loginAttempt.password !== 'ananas') {
-		return res.status(401).send('Wrong username or password');
-	}
+	// 1. hent ut bruker fra databasen
+	User.findOne({username: loginAttempt.username}, function(err, user) {
 
-	var payload = {};
-	var token = jwt.encode(payload, secret);
+		// 1.5 hvis den ikke finnes, unauth
+		if (!user) {
+			return res.status(401).send('Wrong username or password');
+		}
 
-	return res.status(201).send(token);
+		// 2. sammenlikn passord
+		if (loginAttempt.password !== user.password) {
+			return res.status(401).send('Wrong username or password');
+		}
+
+		// 3. bygg og returner token
+		var payload = {username: loginAttempt.username};
+		var token = jwt.encode(payload, secret);
+		
+		return res.status(201).send(token);
+	});
 });
 
 app.get('/api/users', function(req, res) {
-	var token = req.header('authorization');
-
-	if (!token) {
-		return res.status(401).send('No token supplied');
-	}
-
-	var payload = jwt.decode(token, secret);
+	var user = validateUser(req);
 
 	res.json([
 		{username: 'theneva', phone: '32478972'},
@@ -48,6 +62,16 @@ app.get('/api/users', function(req, res) {
 		{username: 'j0nas', phone: '37284923'}
 	]);
 });
+
+function validateUser(req) {
+	var token = req.header('authorization');
+
+	if (!token) {
+		return res.status(401).send('No token supplied');
+	}
+
+	var payload = jwt.decode(token, secret);
+}
 
 var port = 6783;
 app.listen(port);
